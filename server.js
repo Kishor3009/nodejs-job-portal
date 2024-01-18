c26afb2804b5d29e82c6760ce5c9f6ba3b304a41
Kishor3009 <119849236+Kishor3009@users.noreply.github.com>
2024-01-18 10:20:19 +0530

    Updated

--- server.js
+++ server.js
@@ -38,7 +38,8 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:8080",
+//        url: "http://localhost:8080",
+          url: "https://nodejs-job-portal-yx1f.onrender.com",
       },
     ],
   },
